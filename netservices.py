8a9b628ef287addccf24d524af19edc7b3f2a69e
Vijay Kumar Banerjee <vijay@rtems.org>
2021-03-02 14:10:06 -0700

    Initial Commit
    This adds the rtems-waf submodule, minimal build infrastructure, and
    testsuite support files.

---
+++ netservices.py
@@ -0,0 +1,74 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+#  Copyright (C) 2022 On-Line Applications Research Corporation (OAR)
+#  Written by Kinsey Moore <kinsey.moore@oarcorp.com>
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+
+from rtems_waf import rtems
+import json
+import os
+
+
+def removeprefix(data, prefix):
+    if data.startswith(prefix):
+        return data[len(prefix):]
+    return data
+
+
+def build(bld):
+
+    arch_lib_path = rtems.arch_bsp_lib_path(bld.env.RTEMS_VERSION,
+                                            bld.env.RTEMS_ARCH_BSP)
+
+    def install_headers(root_path):
+        for root, dirs, files in os.walk(root_path):
+            for name in files:
+                ext = os.path.splitext(name)[1]
+                src_root = os.path.split(root)
+                path = os.path.join(src_root[0], src_root[1])
+                if ext == '.h':
+                    subpath = removeprefix(removeprefix(path, root_path), "/")
+                    bld.install_files(
+                        os.path.join("${PREFIX}",
+                                     arch_lib_path,
+                                     "include",
+                                     subpath),
+                        os.path.join(path, name)
+                    )
+
+    lib_path = os.path.join(bld.env.PREFIX, arch_lib_path)
+    bld.read_stlib('lwip', paths=[lib_path])
+    bld.read_stlib('rtemstest', paths=[lib_path])
+
+
+def add_flags(flags, new_flags):
+    for flag in new_flags:
+        if flag not in flags:
+            flags.append(flag)
+
+
+def bsp_configure(conf, arch_bsp):
+    conf.env.LIB += ['m']
+    section_flags = ["-fdata-sections", "-ffunction-sections"]
+    add_flags(conf.env.CFLAGS, section_flags)
+    add_flags(conf.env.CXXFLAGS, section_flags)
